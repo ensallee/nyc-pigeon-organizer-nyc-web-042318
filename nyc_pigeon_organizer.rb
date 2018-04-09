f90a3296350e015327a3a8393dc2f47260a3cc8f
ensallee <ensallee@gmail.com>
2018-04-09 16:52:31 -0400

    pigeons

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash={}
+  data.each do |key, value|
+    value.each do |key_2, name_array|
+      name_array.each do |name|
+        new_hash[name] ||= {}
+        new_hash[name][key] ||= []
+        new_hash[name][key] << key_2.to_s
+      end
+    end
+  end
+  new_hash
 end
